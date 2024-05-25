477d3ad58349e30f0aedf7ceb30f2d04714ec475
peacher <1665173066@qq.com>
2024-05-26 06:37:12 +0800

    新增订单相关代码和任务调度相关代码

---
+++ sky-server/src/main/java/com/sky/task/MyTask.java
@@ -0,0 +1,20 @@
+package com.sky.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Component
+@Slf4j
+public class MyTask {
+    /**
+     * 定时任务
+     */
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void executeTask() {
+        System.out.println(LocalDateTime.now());
+    }
+}
